ddc0fde0ec327f01d9324ff8d479095d594b030c
josh-gree <joshua.greenhalgh@mobkoi.com>
2023-02-10 13:05:51 +0000

    setup google provider and create a bucket resource

--- main.tf
+++ main.tf
@@ -26,3 +26,19 @@ world!!!
 This is a new file
 EOT
 }
+
+## remote state
+
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "default" {
+  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+  force_destroy = false
+  location      = "EU"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+}
